0f72a11774ac8c4cdb7df739356acfe0d11092e0
Gidrafff <gidtaker@gmail.com>
2023-02-21 12:35:47 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet_x10 - Make alphabet x10 times
-	 * Return: Always 0.
-	 */
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ * Return: Always 0.
+ */
 
 void print_alphabet_x10(void)
 {
