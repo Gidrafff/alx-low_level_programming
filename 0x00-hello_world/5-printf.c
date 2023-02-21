afe3c949ea617bd4d869c26973dc558f4e2db0c1
Gidrafff <gidtaker@gmail.com>
2023-02-20 18:02:44 -0800

    print 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is
+ * main -Entry point prints "with proper grammar, but the outcome is
  * a piece of art," followed newline
  * Return always 0.
  *
