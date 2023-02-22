bb1c2b01b266b95f08108d37a7a2c0fe0f776a97
Gidrafff <gidtaker@gmail.com>
2023-02-21 18:55:51 -0800

    9nie

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-	int times_table(void)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
 	times_table();
 	return (0);
