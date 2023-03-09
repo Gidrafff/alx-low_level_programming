248ba6446f5134eff98393d60d2f3200a33abdde
Gidrafff <gidtaker@gmail.com>
2023-03-08 23:33:28 -0800

    9timest

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+#include <stdio.h>
+int times_table(void)
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+int main(void) =
 {
+
 	times_table();
-	return (0);
+	return (0); {}
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,7 +16,7 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void times_tablet(void);
+int times_tablet(void);
 
 int add(int, int);
 
