22e9b04e119465aee331a1f39fb88c29f66e4e93
Gidrafff <gidtaker@gmail.com>
2023-03-23 20:24:25 -0700

    h

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 					break;
 				case 's':
 					str = va_arg(list, char *);
-					
+
 					if (!str)
 						str = "(nil)";
 					printf("%s%s", sep, str);
