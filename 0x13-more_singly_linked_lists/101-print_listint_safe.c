eec3630d583856613fb97f24eb4644c4e26601c7
Gidrafff <gidtaker@gmail.com>
2023-04-03 16:06:39 -0700

    o

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,13 +16,13 @@ size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
-	
+
 	if (head == NULL || head->next == NULL)
 		return (0);
-	
+
 	tortoise = head->next;
 	hare = (head->next)->next;
-	
+
 	while (hare)
 	{
 		if (tortoise == hare)
@@ -34,21 +34,21 @@ size_t looped_listint_len(const listint_t *head)
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
-			
+
 			tortoise = tortoise->next;
 			while (tortoise != hare)
 			{
 				nodes++;
 				tortoise = tortoise->next;
 			}
-			
+
 			return (nodes);
 		}
-		
+
 		tortoise = tortoise->next;
 		hare = (hare->next)->next;
 	}
-	
+
 	return (0);
 }
 
@@ -72,7 +72,7 @@ size_t print_listint_safe(const listint_t *head)
 			head = head->next;
 		}
 	}
-	
+
 	else
 	{
 		for (index = 0; index < nodes; index++)
@@ -83,6 +83,6 @@ size_t print_listint_safe(const listint_t *head)
 
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-	
+
 	return (nodes);
 }
